c7883e740bb18a5b9f44be09771fc0e2e501601d
Jose Luis <jolui334@gmail.com>
2021-05-09 11:49:22 -0500

    SVG witdh and height variables created. Also SVG group has been updated

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,20 @@
 // @TODO: YOUR CODE HERE!
+let svgWidth = 800;
+let svgHeigth = 400;
+
+let margin = {
+    top:20,
+    right: 40, 
+    bottom: 60,
+    left: 100
+}
+
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeigh - margin.top - margin.bottom;
+
+// Creating the SVG wrapper to append the SVG gorup which will hold the chart
+
+let  svg = d3.select(".scatter") 
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("heigh", svgHeigth);
